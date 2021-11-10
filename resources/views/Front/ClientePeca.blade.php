413f6104b552e964f4476bb7d61e7990e308b195
KlecioHenrique <kleciohenrique18@gmail.com>
2021-11-10 11:23:06 -0300

    dev

--- resources/views/Front/ClientePeca.blade.php
+++ resources/views/Front/ClientePeca.blade.php
@@ -108,7 +108,7 @@
 
       {{-- PARTE DO CARRINHO DE COMPRAS --}}
 
-      <pre><button class="button button1" onclick="Salvar()" >ADICIONAR AO CARRINHO</button><button class="button button3">CONSULTOR DE VENDA</button></pre><br>
+      <pre><button class="button button1" onclick="Salvar()" >ADICIONAR AO CARRINHO</button><button class="button button3" onclick="consultor()">CONSULTOR DE VENDA</button></pre><br>
 
       {{-- PARTE DO CARRINHO LISTA DE PRODUTOS --}}
     <div id="refresh">
@@ -432,8 +432,13 @@
         const encoded = encodeURI(uri);
         numero = "5581995247120"
         var edit = `https://api.whatsapp.com/send?phone=${numero}&text=${encoded} `
-        console.log(edit)
-        location.href = editado
+        location.href = edit
+        }
+
+        function consultor()
+        {
+          var edit = `https://api.whatsapp.com/send?phone=5581995247120&text=preciso%20de%20ajuda`
+          location.href = edit
         }
       </script>
 </body>
